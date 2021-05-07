f9eb7553b8e8933a1887d63d91dd9a2a6fa143e8
zhuyuanmin <yuanmin.zhu@wetax.com.cn>
2021-05-07 11:29:55 +0800

    xxx

--- src/pages/page/index.jsx
+++ src/pages/page/index.jsx
@@ -5,10 +5,12 @@ const aa = () => '王八蛋'
 
 export default class Page extends PureComponent {
   render() {
-    return <div>
-      this is Page~ 呵呵  {aa()}
-      {/* <img src={require('@/images/photo.jpg')} alt="" srcSet="" /> */}
-      <ReactSVG src={require('@/images/reject-icon.svg')} />
+    return (
+      <div>
+        this is Page~ 呵呵 {aa()}
+        {/* <img src={require('@/images/photo.jpg')} alt="" srcSet="" /> */}
+        <ReactSVG src={require('@/images/reject-icon.svg')} />
       </div>
+    )
   }
 }
