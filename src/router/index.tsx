bd7cdd3fae0a5aaaa4fd074b7ed6082f2b37fb14
zhuyuanmin <yuanmin.zhu@wetax.com.cn>
2021-05-08 13:53:58 +0800

    fixed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,13 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 import loadable from '@/utils/loadable'
 
 // 路由
-export default function RouteList() {
-  return (
-    <Switch>
-      <Route exact path="/" component={loadable('/home')} />
-      <Route path="/page" component={loadable('/page')} />
-      <Route path="/counter" component={loadable('/counter')} />
-      <Route component={() => <div>404 NotFound !</div>}/>
-    </Switch>
-  )
-}
+export default () => (
+  <Switch>
+    <Route exact path="/" component={loadable('/home')} />
+    <Route path="/page" component={loadable('/page')} />
+    <Route path="/counter" component={loadable('/counter')} />
+    <Route component={() => <div>404 NotFound !</div>} />
+  </Switch>
+)
